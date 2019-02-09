9858e87ed9221f48cf2e9cef0197116d7d179673
ivan Jankovic <jankoemail@gmail.com>
2019-02-08 21:50:48 -0500

    main page done

--- hairstylist_ry/lib/main.dart
+++ hairstylist_ry/lib/main.dart
@@ -29,7 +29,8 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         elevation: 0.0,
         backgroundColor: Colors.white,
-        title: Text('BOOKING',
+        title: Text(
+          'BOOKING',
           style: TextStyle(
             fontFamily: 'FirSans',
             fontSize: 16.0,
@@ -62,28 +63,244 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               Positioned(
                 top: 20.0,
-                left: 15.0,
-                right: 15.0,
+                left: 12.0,
+                right: 5.0,
                 child: Container(
                   height: 60.0,
                   child: ListView(
                     scrollDirection: Axis.horizontal,
                     children: <Widget>[
                       getDate(18, 'Tue'),
-                      SizedBox(width: 25.0),
+                      SizedBox(width: 20.0),
                       getDate(19, 'Wed'),
-                      SizedBox(width: 25.0),
+                      SizedBox(width: 20.0),
                       getDate(20, 'Thu'),
-                      SizedBox(width: 25.0),
+                      SizedBox(width: 20.0),
                       getDate(21, 'Fri'),
-                      SizedBox(width: 25.0),
+                      SizedBox(width: 20.0),
+                      getDate(22, 'Sat'),
                     ],
                   ),
                 ),
               )
             ],
           ),
+          SizedBox(height: 35.0),
+          Center(
+            child: Text(
+              'Hagorapt',
+              style: TextStyle(
+                letterSpacing: 2.0,
+                fontFamily: 'Nunito',
+                fontSize: 30.0,
+                color: Colors.black.withOpacity(0.6),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 15.0),
+          Row(
+            children: <Widget>[
+              SizedBox(width: 35.0),
+              getService('Beards', 50),
+              getService('Crew Cut', 15),
+            ],
+          ),
+          SizedBox(height: 15.0),
+          Container(
+            height: 175.0,
+            child: ListView(
+              padding: EdgeInsets.only(
+                left: 15.0,
+                right: 15.0,
+              ),
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                getBarber('assets/style2.jpg', 'Slavoljub'),
+                SizedBox(width: 15.0),
+                getBarber('assets/style1.jpg', 'Milivoje'),
+                SizedBox(width: 15.0),
+                getBarber('assets/style3.jpeg', 'Zavisa'),
+                SizedBox(width: 15.0),
+              ],
+            ),
+          ),
+          SizedBox(height: 25.0),
+          Container(
+            height: 42.0,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                getTime('11.00'),
+                SizedBox(width: 20.0),
+                getTime('12.30'),
+                SizedBox(width: 20.0),
+                getTime('13.30'),
+                SizedBox(width: 20.0),
+                getTime('15.00'),
+                SizedBox(width: 20.0),
+                getTime('16.30'),
+              ],
+            ),
+          ),
+          SizedBox(height: 25.0),
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+            child: InkWell(
+              onTap: () {},
+              child: Container(
+                height: 42.0,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(7.0),
+                    color: Colors.black),
+                child: Center(
+                  child: Text(
+                    'BOOK',
+                    style: TextStyle(
+                        letterSpacing: 2.0,
+                        fontFamily: 'FirSans',
+                        fontSize: 15.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
+  Color switchTimeColor(time) {
+    if (time == selectedTime) {
+      return Colors.black.withOpacity(0.8);
+    } else {
+      return Colors.grey.withOpacity(0.2);
+    }
+  }
+
+  Color switchTimeContentColor(time) {
+    if (time == selectedTime) {
+      return Colors.white;
+    } else {
+      return Colors.black;
+    }
+  }
+
+  selectTime(time) {
+    setState(() {
+      selectedTime = time;
+    });
+  }
+
+  Widget getTime(time) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 500),
+      curve: Curves.easeIn,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: switchTimeColor(time)),
+      height: 42.0,
+      width: 72.0,
+      child: InkWell(
+        onTap: () {
+          selectTime(time);
+        },
+        child: Center(
+          child: Text(
+            time,
+            style: TextStyle(
+                fontFamily: 'Nunito',
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold,
+                color: switchTimeContentColor(time)),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget getBarber(String imgPath, String name) {
+    return Column(
+      children: <Widget>[
+        Stack(
+          children: <Widget>[
+            Container(
+              height: 150.0,
+              width: 150.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(7.0),
+                image: DecorationImage(
+                  image: AssetImage(imgPath),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                selectBarber(name);
+              },
+              child: Container(
+                height: 150.0,
+                width: 150.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(7.0),
+                  color: getSelectedBarber(name),
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 7.0),
+        Text(
+          name,
+          style: TextStyle(
+            fontFamily: 'FirSans',
+            fontSize: 15.0,
+          ),
+        ),
+      ],
+    );
+  }
+
+  selectBarber(name) {
+    setState(() {
+      selectedBarber = name;
+    });
+  }
+
+  Color getSelectedBarber(name) {
+    print(selectedBarber);
+    print(selectedDate);
+    print(selectedTime);
+    if (name == selectedBarber) {
+      return Colors.green.withOpacity(0.3);
+    } else {
+      return Colors.transparent;
+    }
+  }
+
+  Widget getService(String name, int price) {
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Text(
+            name,
+            style: TextStyle(
+                fontFamily: 'Nunito', fontSize: 17.0, color: Colors.black),
+          ),
+          SizedBox(width: 5.0),
+          Text(
+            '\$' + price.toString(),
+            style: TextStyle(
+                fontFamily: 'Nunito', fontSize: 17.0, color: Colors.grey),
+          ),
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () {},
+          )
         ],
       ),
     );
@@ -91,12 +308,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget getDate(int date, String day) {
     return AnimatedContainer(
-      duration: Duration(milliseconds: 500),
+      duration: Duration(milliseconds: 200),
       curve: Curves.easeIn,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30.0),
-        color: switchColor(date)
-      ),
+          borderRadius: BorderRadius.circular(30.0), color: switchColor(date)),
       width: 60.0,
       height: 60.0,
       child: InkWell(
@@ -116,7 +331,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   color: switchContentColor(date),
                 ),
               ),
-              SizedBox(height: 3.0),
+              SizedBox(height: 2.0),
               Text(
                 day,
                 style: TextStyle(
